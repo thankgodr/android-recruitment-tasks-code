9982ecd88118c7603a19285c2bce9941b08f89a7
ThankGod Richard <madueketf@gmail.com>
2022-04-29 02:13:14 +0100

    Revert "Added and run git code_review_pr.diff"
    This reverts commit 4f59bb1d6aed8a7ab1c229f9f54f0642fe73b4a0.

--- app/src/main/java/com/netguru/codereview/MainActivity.kt
+++ app/src/main/java/com/netguru/codereview/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.netguru.codereview
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.netguru.codereview.ui.MainFragment
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment()).commitNow()
     }
 }
